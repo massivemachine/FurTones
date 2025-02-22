80ab41878cc79a6d5093ae18c18d5d463dd3e4fd
Max Carmona <psymc11@nottingham.ac.uk>
2025-02-22 16:12:55 +0000

    note image

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 Array.from(document.getElementsByClassName("note")).forEach(function (note){
     note.addEventListener("click", function() {
         console.log(note.id);
+        document.getElementById(note.id).innerHTML = "<img src=/assets/notes/blue.png>";
     });
 });
