920e9a34851a64169e06fb6d2a3b777af36ff55e
Max Carmona <psymc11@nottingham.ac.uk>
2025-02-22 16:22:05 +0000

    resize

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 Array.from(document.getElementsByClassName("note")).forEach(function (note){
     note.addEventListener("click", function() {
         console.log(note.id);
-        document.getElementById(note.id).innerHTML = "<img src=/assets/notes/blue.png>";
+        document.getElementById(note.id).innerHTML = "<img src=\"/assets/notes/blue.png\" width=\"50px\" height=\"30px\">";
     });
 });
