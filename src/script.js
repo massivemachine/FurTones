aa13c76fe66b9de91325ac82b7901029112a221b
Max Carmona <psymc11@nottingham.ac.uk>
2025-02-22 17:19:50 +0000

    disappearing notes

--- src/script.js
+++ src/script.js
@@ -1,13 +1,33 @@
 var note_grid = [[],[]];
 
+
 Array.from(document.getElementsByClassName("note")).forEach(function (note){
+    note_grid[noteIndex(note.id)] = 0;
     note.addEventListener("click", function() {
-        console.log(note.id);
-        document.getElementById(note.id + "-im").innerHTML = "<img src=\"/assets/notes/blue.png\" width=\"50px\" height=\"30px\">";
+        console.log(note.id,note_grid[noteIndex(note.id)]);
+        if (note_grid[noteIndex(note.id)] == 0) {
+            note_grid[noteIndex(note.id)] = 1;
+            document.getElementById(note.id + "-im").innerHTML = "<img src=\"/assets/notes/blue.png\" width=\"70px\" height=\"40px\">";
+        } else {
+            note_grid[noteIndex(note.id)] = 0;
+            document.getElementById(note.id + "-im").innerHTML = "";
+        }
+        
     });
 });
 
-function noteToCoord(id) {
-    coord = [];
-    coord[0] = id.splice(0,1)
+
+function noteIndex(id) {
+    var coord = [];
+    switch (id.slice(0,1)) {
+        case "e": coord[0] = 0; break;
+        case "d": coord[0] = 1; break;
+        case "c": coord[0] = 2; break;
+        case "b": coord[0] = 3; break;
+        case "a": coord[0] = 4; break;
+        case "g": coord[0] = 5; break;
+        case "f": coord[0] = 6;
+    }
+    coord[1] = id.slice(1,2);
+    return coord;
 }
