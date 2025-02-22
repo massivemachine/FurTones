bf30b49e901ba07af8c96defb498a2192585a9c5
Max Carmona <psymc11@nottingham.ac.uk>
2025-02-22 16:38:43 +0000

    update images when click

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
+note_grid = [[],[]];
+
 Array.from(document.getElementsByClassName("note")).forEach(function (note){
     note.addEventListener("click", function() {
         console.log(note.id);
-        document.getElementById(note.id).innerHTML = "<img src=\"/assets/notes/blue.png\" width=\"50px\" height=\"30px\">";
+        document.getElementById(note.id + "-im").innerHTML = "<img src=\"/assets/notes/blue.png\" width=\"50px\" height=\"30px\">";
     });
 });
+
+function noteToCoord(id) {
+
+}
