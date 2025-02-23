b704636be847330e56c4d7549cd8118dddd5cade
psyos9 <psyos9@exmail.nottingham.ac.uk>
2025-02-23 11:12:44 +0000

    try this

--- src/server.py
+++ src/server.py
@@ -1,25 +1,24 @@
 from flask import Flask, request, make_response, render_template
 from keyboard import play_notes, setup, stop_motors
 
+
 app = Flask(__name__, template_folder=".",static_folder="./assets")
 setup()
+try:
+    @app.get("/")
+    def render_app():
+        return make_response(render_template("index.html"))
 
-@app.get("/")
-def render_app():
-    return make_response(render_template("index.html"))
-
-@app.post("/")
-def send_to_keyboard():
-    note_json = request.get_json()
-    note_array = note_json["played_notes"]
-    print(note_array)
-    play_notes(note_array)
+    @app.post("/")
+    def send_to_keyboard():
+        note_json = request.get_json()
+        note_array = note_json["played_notes"]
+        print(note_array)
+        play_notes(note_array)
 
 
-@app.post("/stop")
-def halt_playing():
-    pass
-
-@app.teardown_appcontext
-def tear_down():
+    @app.post("/stop")
+    def halt_playing():
+        pass
+except KeyboardInterrupt:
     stop_motors()
