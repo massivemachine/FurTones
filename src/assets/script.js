6fdb81aba95908bbf2268d635ca4abd54faa436a
Max Carmona <psymc11@nottingham.ac.uk>
2025-02-23 12:04:48 +0000

    ip

--- src/assets/script.js
+++ src/assets/script.js
@@ -193,7 +193,7 @@ function getNotes() {
         }
     }
 
-    fetch("//10.229.117.199:5000", {
+    fetch("//10.229.116.123:5000", {
         method: "POST",
         body: JSON.stringify({
         title: "Play",
